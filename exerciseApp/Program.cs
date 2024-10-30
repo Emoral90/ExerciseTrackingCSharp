a36febebe5f67f00da8c8decac699ef2a69aee9d
Erick <moralerick90@gmail.com>
2024-10-29 18:12:30 -0700

    added program entry

--- exerciseApp/Program.cs
+++ exerciseApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create activities
+        Activity running = new Running(new DateTime(2022, 11, 3), 30, 4.8);
+        Activity cycling = new Cycling(new DateTime(2022, 11, 4), 45, 20.0);
+        Activity swimming = new Swimming(new DateTime(2022, 11, 5), 60, 30);
+
+        // Add activities to a list
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        // Display summaries for each activity
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
